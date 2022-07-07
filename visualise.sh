d1d394565edb9eb8a9d592e6aee808f590d88cdf
418Coffee <matthijskes@gmail.com>
2022-07-07 22:26:05 +0200

    Update visualise.sh

--- visualise.sh
+++ visualise.sh
@@ -6,7 +6,7 @@ export SAVE_WEIGHTS=1
 
 rm -rf data
 v run .
-./ffmpeg.exe -y -i data/weights-%05d.ppm temp.mp4
-./ffmpeg.exe -y -i temp.mp4 -vf palettegen data/palette.png
-./ffmpeg.exe -y -i temp.mp4 -i data/palette.png -filter_complex paletteuse adjustments.gif
+ffmpeg -y -i data/weights-%05d.ppm temp.mp4
+ffmpeg -y -i temp.mp4 -vf palettegen data/palette.png
+ffmpeg -y -i temp.mp4 -i data/palette.png -filter_complex paletteuse adjustments.gif
 rm temp.mp4
